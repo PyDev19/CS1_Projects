b4588f99259c6ca9ba3670754cec4143013d09b6
Atharva Mishra <atharvamishra956+1@gmail.com>
2024-01-23 21:48:40 -0600

    added Bulleyes project for StdDraw lesson

--- Unit_5/src/Bullseye.java
+++ Unit_5/src/Bullseye.java
@@ -1,4 +1,5 @@
 import StdDraw.StdDraw;
+import java.awt.Color;
 
 public class Bullseye {
     public static void main(String[] args) {
@@ -7,5 +8,13 @@ public class Bullseye {
         StdDraw.setCanvasSize(width, height);
         StdDraw.setXscale(0, width);
         StdDraw.setYscale(0, height);
+
+        int[] radii = {300, 225, 175, 100, 50};
+        Color[] colors = {StdDraw.BLACK, StdDraw.WHITE, StdDraw.BLUE, StdDraw.RED, StdDraw.YELLOW};
+
+        for (int i = 0; i < radii.length; i++) {
+            StdDraw.setPenColor(colors[i]);
+            StdDraw.filledCircle(400, 300, radii[i]);
+        }
     }
 }
