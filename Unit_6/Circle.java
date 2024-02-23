a865d168df97b6307d7ae5a90f7a8414ebcd7bf9
Atharva Mishra <atharvamishra956+1@gmail.com>
2024-02-23 17:46:12 -0600

    Add Circle class with area and circumference calculations

---
+++ Unit_6/Circle.java
@@ -0,0 +1,15 @@
+public class Circle {
+    public double radius;
+
+    public Circle(double r) {
+        radius = r;
+    }
+
+    public double area() {
+        return  Math.PI * radius * radius;
+    }
+
+    public double circumference() {
+        return 2 * Math.PI * radius;
+    }
+}
